c472dc59dc23594dec2dbed568d165a51e12dd75
Jerson_Ferrer <jferrer00747@gmail.com>
2020-10-21 19:23:16 -0500

    Se agregaron y modificaron los controladores de usuario

---
+++ controler/action/act_registrarUsuario.php
@@ -0,0 +1,37 @@
+<?php
+   
+    session_start();
+    
+    require_once (__DIR__."/../mdb/mdbUsuario.php");
+    require_once (__DIR__."/../../model/entity/Usuario.php");
+
+        $nroCedula = " ";
+        $nombres = $_POST['names'];
+        $apellidos = $_POST['last_names'];
+        $correo = $_POST['email'];
+        $password = $_POST['password'];
+        $repeatPassword = $_POST['repeat_password'];
+        $direccion = " ";
+        $telefono = "";
+ 
+
+        if($password == $repeatPassword){
+            $usuario = new Usuario(NULL, $nroCedula, $nombres, $apellidos, $correo, $password, $direccion, $telefono);
+            registrarUsuario($usuario);
+            header("Location: ../../view/login.php");
+        }else{
+            echo "Contraseñas no coinciden";
+        }
+        /*if(isset($_POST['administrador'])) {
+            $usuario = new Usuario(NULL, $nombre, $correo, $password, $telefono, $fechanac, $sexo, $peso, $administrador);
+            registrarUsuario($usuario);
+            header("Location: ../../vista/administradorUsuarios.php");
+        }else{
+            $usuario = new Usuario(NULL, $nombre, $correo, $password, $telefono, $fechanac, $sexo, $peso, 0);
+            registrarUsuario($usuario);
+            header("Location: ../../vista/login.php");
+        }*/
+
+        
+        
+
--- view/register.php
+++ view/register.php
@@ -66,34 +66,34 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Crea una cuenta!</h1>
                             </div>
-                            <form class="user">
+                            <form class="user" method="POST" action="../controler/action/act_registrarUsuario.php">
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                            placeholder="Nombres">
+                                        <input type="text" class="form-control form-control-user" id="Names"
+                                            placeholder="Nombres" name="names">
                                     </div>
                                     <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                            placeholder="Apellidos">
+                                        <input type="text" class="form-control form-control-user" id="LastNames"
+                                            placeholder="Apellidos" name="last_names">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                        placeholder="Email">
+                                    <input type="email" class="form-control form-control-user" id="nputEmail"
+                                        placeholder="Email" name="email">
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user"
-                                            id="exampleInputPassword" placeholder="Contraseña">
+                                            id="InputPassword" placeholder="Contraseña" name="password">
                                     </div>
                                     <div class="col-sm-6">
                                         <input type="password" class="form-control form-control-user"
-                                            id="exampleRepeatPassword" placeholder="Repita la contraseña">
+                                            id="RepeatPassword" placeholder="Repita la contraseña" name="repeat_password">
                                     </div>
                                 </div>
-                                <a href="login.php" class="btn btn-primary btn-user btn-block">
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
                                     Crear cuenta
-                                </a>
+                                </button>
                                 <hr>
                                 <a href="index.php" class="btn btn-google btn-user btn-block">
                                     <i class="fab fa-google fa-fw"></i> Registrarse con Google
@@ -115,7 +115,7 @@
             </div>
         </div>
     </div>
-    
+
     <!-- Footer-->
     <footer class="footer py-4 bg-light">
         <div class="container">
