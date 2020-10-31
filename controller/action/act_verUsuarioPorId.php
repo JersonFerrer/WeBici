03de393fa3deb308302de4e9c826630cc090bb2c
Jerson_Ferrer <jferrer00747@gmail.com>
2020-10-30 20:58:31 -0500

    Se cambió la forma de acceder a los datos de usuario

--- controller/action/act_verUsuarioPorId.php
+++ controller/action/act_verUsuarioPorId.php
@@ -1,11 +1,9 @@
 <?php
-    //session_start();
+    session_start();
     require_once (__DIR__.'/../mdb/mdbUsuario.php');
     
-    //$idUsuario = $_POST['idUsuario'];
-    $idUsuario = $_SESSION['ID_USUARIO'];
+    $idUsuario = $_POST['idUsuario'];
 
     $usuario = verUsuarioPorId($idUsuario);
-
-    json_encode($usuario);
-    header('location: ../../view/index.php');
+    
+    $jsonUsuario = json_encode($usuario);
