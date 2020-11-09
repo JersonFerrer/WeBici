19f2083ad72b1d5ff09ae153f954791e5bf96be2
Jerson_Ferrer <jferrer00747@gmail.com>
2020-11-08 20:19:11 -0500

    Se agregó para mostrar las bicis de ruta

--- controller/mdb/mdbBicicleta.php
+++ controller/mdb/mdbBicicleta.php
@@ -1,11 +1,11 @@
 <?php
 require_once(__DIR__."/../../model/dao/BicicletaDAO.php");
 
-function verBicicletas(){
+function verBicicletas($tipo){
 
     $bicicleta = new BicicletaDAO();
 
-    $bicicletas = $bicicleta->verBicicletas();
+    $bicicletas = $bicicleta->verBicicletas($tipo);
 
     return $bicicletas;
 }
